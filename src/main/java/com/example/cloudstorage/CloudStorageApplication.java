a4982c51f0974d78fa5621427e029e31f8e56ab1
bloodypenelope <bloodypenelope@gmail.com>
2024-03-26 14:58:51 +0500

    Added simple REST API for storing files

--- src/main/java/com/example/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/example/cloudstorage/CloudStorageApplication.java
@@ -1,13 +1,25 @@
 package com.example.cloudstorage;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.cloudstorage.properties.StorageProperties;
+import com.example.cloudstorage.service.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class CloudStorageApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
+	}
 }
