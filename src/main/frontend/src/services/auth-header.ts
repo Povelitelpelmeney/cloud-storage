425e8d537cf3bfd78d23bfb92bef210833761c4c
Давид Шамугия <bloodypenelope@gmail.com>
2024-05-05 20:40:46 +0500

    deleted frontend

--- src/main/frontend/src/services/auth-header.ts
+++
@@ -1,7 +0,0 @@
-export default function authHeader() {
-  const userStr = localStorage.getItem("user");
-  let user = null;
-  if (userStr) user = JSON.parse(userStr);
-  const token = user?.accessToken ? `Bearer ${user.accessToken}` : ""
-  return { Authorization: token }
-}
--- src/main/frontend/src/types/user-type.ts
+++
@@ -1,7 +0,0 @@
-export default interface IUser {
-  id?: number;
-  username?: string;
-  email?: string;
-  password?: string;
-  // roles?: Array<string>;
-}
